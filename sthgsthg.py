7fb742747cefe0fc9015ed5016c7091d8319c633
sleekEagle <lahirunuwanisme@gmail.com>
2026-01-19 19:23:20 +0530

    somthing somehting dataset model from huggingface test on dummy data

--- sthgsthg.py
+++ sthgsthg.py
@@ -1,1 +1,28 @@
-https://huggingface.co/MCG-NJU/videomae-base-finetuned-ssv2
+# https://huggingface.co/MCG-NJU/videomae-base-finetuned-ssv2
+
+
+from transformers import VideoMAEImageProcessor, VideoMAEForVideoClassification
+import numpy as np
+import torch
+
+'''
+dtype = np.uint8
+range = [0, 255]
+shape = (H, W, 3)
+'''
+video = np.random.randint(
+    0, 256, size=(16, 224, 224, 3), dtype=np.uint8
+)
+video = list(video)  # list of frames
+
+processor = VideoMAEImageProcessor.from_pretrained("MCG-NJU/videomae-base-finetuned-ssv2")
+model = VideoMAEForVideoClassification.from_pretrained("MCG-NJU/videomae-base-finetuned-ssv2")
+
+inputs = processor(video, return_tensors="pt")
+
+with torch.no_grad():
+  outputs = model(**inputs)
+  logits = outputs.logits
+
+predicted_class_idx = logits.argmax(-1).item()
+print("Predicted class:", model.config.id2label[predicted_class_idx])
