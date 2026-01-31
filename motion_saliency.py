9a6530e4d8ef33f464c95822ad70db46541a5f76
sleekEagle <lahirunuwanisme@gmail.com>
2026-01-31 11:46:55 +0530

    updates

--- motion_saliency.py
+++ motion_saliency.py
@@ -10,6 +10,7 @@ model.to('cuda')
 model.eval()
 inference_loader = ucf101dm.inference_loader
 class_names = ucf101dm.inference_class_names
+THR = 0.05
 
 
 if __name__ == '__main__':
@@ -20,13 +21,32 @@ if __name__ == '__main__':
 
     for k in data_dict:
         d = data_dict[k]
+
+        #consider only samples that are correcly predicted
+        gt_class = d['motion_importance']['gt_class']
+        pred_class = d['motion_importance']['pred_original_class']
+        if gt_class.lower() != pred_class.lower():
+            continue
+
         #is there a single frame that explains the whole video ?
         sfs = d['single_frame_structure']
         if sfs:
             print('There is a single frame that can explain the whole video well')
+            continue
         else:
             # what sub set of frames can be used to explain the whole video ?
-            d
+            clustered_ids = d['pair_analysis']['clustered_ids']
+            pair_importance = d['pair_analysis']['pair_importance'] # this is percentage change. lower, better
+
+            if pair_importance[0][0] == [None,None]:
+                if pair_importance[0][1] < THR:
+                    print('Motion is not important for this video')
+                    continue
+            pass
+
+
+
+
 
     
 
