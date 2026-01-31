a56cbfe0ce6843c9cf52335b4de825a69faf1b35
sleekEagle <lahirunuwanisme@gmail.com>
2026-01-31 10:20:23 +0530

    starting working on motion saliency code

---
+++ motion_saliency.py
@@ -0,0 +1,37 @@
+import func
+import json
+
+
+
+#create model and data loader
+ucf101dm = func.UCF101_data_model()
+model = ucf101dm.model
+model.to('cuda')
+model.eval()
+inference_loader = ucf101dm.inference_loader
+class_names = ucf101dm.inference_class_names
+
+
+if __name__ == '__main__':
+
+    path = r'C:\Users\lahir\Downloads\UCF101\analysis\motion_importance.json'
+    with open(path, 'r', encoding='utf-8') as file:
+        data_dict = json.load(file)
+
+    for k in data_dict:
+        d = data_dict[k]
+        #is there a single frame that explains the whole video ?
+        sfs = d['single_frame_structure']
+        if sfs:
+            print('There is a single frame that can explain the whole video well')
+        else:
+            # what sub set of frames can be used to explain the whole video ?
+            d
+
+    
+
+
+
+
+
+    gmodel = func.GradcamModel(model)
