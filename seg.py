361116aa8dca220a87b0f6543eed424cff9b2aa8
sleekEagle <lahirunuwanisme@gmail.com>
2026-02-13 20:32:53 +0530

    updates

--- seg.py
+++ seg.py
@@ -28,79 +28,93 @@ for future: use EntitySAM: Segment Everything in Video
 problem: very hard to install it (detectron 2 required for it in windows)
 '''
 
+class Seg:
+    def __init__(self):
+        self.image_processor = AutoImageProcessor.from_pretrained("facebook/mask2former-swin-small-cityscapes-panoptic")
+        self.model = Mask2FormerForUniversalSegmentation.from_pretrained(
+            "facebook/mask2former-swin-small-cityscapes-panoptic"
+        )
+    
+    def get_segmentation_path(self, img_path):
+        image = Image.open(img_path)
+        inputs = self.image_processor(image, return_tensors="pt")
+        with torch.no_grad():
+            outputs = self.model(**inputs)
+        result = self.image_processor.post_process_panoptic_segmentation(
+            outputs, target_sizes=[(image.height, image.width)]
+        )[0]["segmentation"]
+
+        mask_ids = torch.unique(result)
+        object_masks = torch.empty(0)
+        for id in mask_ids:
+            m = torch.zeros_like(result)
+            m[result==id] = 1
+            object_masks = torch.concat([object_masks, m.unsqueeze(0)], dim=0)
+
+        return object_masks
+    
+    def get_segmentation(self, image):
+        inputs = self.image_processor(image, return_tensors="pt")
+        with torch.no_grad():
+            outputs = self.model(**inputs)
+        result = self.image_processor.post_process_panoptic_segmentation(
+            outputs, target_sizes=[(image.height, image.width)]
+        )[0]["segmentation"]
+
+        mask_ids = torch.unique(result)
+        object_masks = torch.empty(0)
+        for id in mask_ids:
+            m = torch.zeros_like(result)
+            m[result==id] = 1
+            object_masks = torch.concat([object_masks, m.unsqueeze(0)], dim=0)
+
+        return object_masks
+
+
 
 if __name__ == '__main__':
-    image_processor = AutoImageProcessor.from_pretrained("facebook/mask2former-swin-small-cityscapes-panoptic")
-    model = Mask2FormerForUniversalSegmentation.from_pretrained(
-        "facebook/mask2former-swin-small-cityscapes-panoptic"
-    )
-    # url = "https://cdn-media.huggingface.co/Inference-API/Sample-results-on-the-Cityscapes-dataset-The-above-images-show-how-our-method-can-handle.png"
-    # with httpx.stream("GET", url) as response:
-    #     image = Image.open(BytesIO(response.read()))
-    image = Image.open(r'C:\Users\lahir\Downloads\UCF101\jpgs\SoccerJuggling\v_SoccerJuggling_g01_c02\image_00150.jpg')
-    image.show()
 
+    seg = Seg()
 
-    inputs = image_processor(image, return_tensors="pt")
+    ucf101dm = func.UCF101_data_model()
+    video = ucf101dm.load_jpg_ucf101(r'C:\Users\lahir\Downloads\UCF101\jpgs\SoccerJuggling\v_SoccerJuggling_g01_c02',n=0)
+    video = video.permute(0,2,3,1)
+    video = (video-video.min())/(video.max()-video.min())*255
+    video = video.int()
 
-    with torch.no_grad():
-        outputs = model(**inputs)
+    # seg.get_segmentation_path(r'C:\Users\lahir\Downloads\UCF101\jpgs\SoccerJuggling\v_SoccerJuggling_g01_c02\image_00392.jpg')
 
-    class_queries_logits = outputs.class_queries_logits
-    masks_queries_logits = outputs.masks_queries_logits
-
-    # Perform post-processing to get panoptic segmentation map
-    result = image_processor.post_process_panoptic_segmentation(
-        outputs, target_sizes=[(image.height, image.width)]
-    )[0]["segmentation"]
-    #generate masks
-    mask_ids = np.unique(result)
-    object_masks = []
-    for id in mask_ids:
-        m = np.zeros_like(result)
-        m[result==id] = 1
-        object_masks.append(m)
-
-    print(result.shape)
+    img = Image.fromarray(video[0,:].to(torch.uint8).numpy())
+    masks = seg.get_segmentation(img)
+    # mask = masks.float()
+    # masks = (masks > 0.5)
+    masks = masks.unsqueeze(1)
+    # masks = masks.to(device="cuda", dtype=torch.bfloat16)
 
     # func.show_gray_image(object_masks[3])
     # func.show_gray_image(result.detach().cpu().numpy())
-    def get_mask_xy(mask, stride=3):
-        H,W = mask.shape
-        stride = 3
-        grid = torch.zeros((H,W))
-        grid[::stride,::stride] = 1
-        grid[::stride,::stride] = 1
-        return torch.nonzero(grid*mask)
-
-    # mxy = get_mask_xy(object_masks[3])
-    
-    # func.show_gray_image(g.detach().cpu().numpy())
-
 
     model = Sam2VideoModel.from_pretrained("facebook/sam2.1-hiera-tiny").to('cuda', dtype=torch.bfloat16)
     processor = Sam2VideoProcessor.from_pretrained("facebook/sam2.1-hiera-tiny")
     
     from transformers.video_utils import load_video
     video_url = "https://huggingface.co/datasets/hf-internal-testing/sam2-fixtures/resolve/main/bedroom.mp4"
-    video_frames, _ = load_video(video_url)
+    video, _ = load_video(video_url)
+
+
 
     inference_session = processor.init_video_session(
-        video=video_frames,
+        video=video,
         inference_device='cuda',
         dtype=torch.bfloat16,
     )
-    inference_session.reset_inference_session()
-    
-    mask = object_masks[3]
-    mask_tensor = torch.tensor(mask, dtype=torch.float32)
-    mask_tensor = mask_tensor.unsqueeze(0).unsqueeze(0)
+    # inference_session.reset_inference_session()
 
     processor.add_inputs_to_inference_session(
         inference_session=inference_session,
         frame_idx=0,
-        obj_ids=[1],  # list of object IDs
-        input_masks=mask_tensor.to('cuda'),
+        obj_ids = [1,2,3,4],
+        input_masks=masks,
     )
 
     video_segments = {}
