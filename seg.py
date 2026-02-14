ed5e9be261b8812701b498117b9f4ec233ae2ca8
sleekEagle <lahirunuwanisme@gmail.com>
2026-02-14 14:32:30 +0530

    delete unnecessary

--- seg.py
+++ seg.py
@@ -77,7 +77,7 @@ if __name__ == '__main__':
     seg = Seg()
 
     ucf101dm = func.UCF101_data_model()
-    video = ucf101dm.load_jpg_ucf101(r'C:\Users\lahir\Downloads\UCF101\jpgs\SoccerJuggling\v_SoccerJuggling_g01_c02',n=0)
+    video = ucf101dm.load_jpg_ucf101(r'C:\Users\lahir\Downloads\UCF101\jpgs\PullUps\v_PullUps_g25_c02',n=0)
     video = video.permute(0,2,3,1)
     video = (video-video.min())/(video.max()-video.min())*255
     video = video.int()
@@ -94,35 +94,116 @@ if __name__ == '__main__':
     # func.show_gray_image(object_masks[3])
     # func.show_gray_image(result.detach().cpu().numpy())
 
-    model = Sam2VideoModel.from_pretrained("facebook/sam2.1-hiera-tiny").to('cuda', dtype=torch.bfloat16)
-    processor = Sam2VideoProcessor.from_pretrained("facebook/sam2.1-hiera-tiny")
+#     model = Sam2VideoModel.from_pretrained("facebook/sam2.1-hiera-tiny").to('cuda', dtype=torch.bfloat16)
+#     processor = Sam2VideoProcessor.from_pretrained("facebook/sam2.1-hiera-tiny")
     
-    from transformers.video_utils import load_video
-    video_url = "https://huggingface.co/datasets/hf-internal-testing/sam2-fixtures/resolve/main/bedroom.mp4"
-    video, _ = load_video(video_url)
+#     from transformers.video_utils import load_video
+#     video_url = "https://huggingface.co/datasets/hf-internal-testing/sam2-fixtures/resolve/main/bedroom.mp4"
+#     video, _ = load_video(video_url)
 
 
 
-    inference_session = processor.init_video_session(
-        video=video,
-        inference_device='cuda',
-        dtype=torch.bfloat16,
-    )
-    # inference_session.reset_inference_session()
+#     inference_session = processor.init_video_session(
+#         video=video,
+#         inference_device='cuda',
+#         dtype=torch.bfloat16,
+#     )
+#     # inference_session.reset_inference_session()
 
-    processor.add_inputs_to_inference_session(
-        inference_session=inference_session,
-        frame_idx=0,
-        obj_ids = [1,2,3,4],
-        input_masks=masks,
-    )
+#     processor.add_inputs_to_inference_session(
+#         inference_session=inference_session,
+#         frame_idx=0,
+#         obj_ids = [1,2,3,4],
+#         input_masks=masks,
+#     )
 
-    video_segments = {}
-    for sam2_video_output in model.propagate_in_video_iterator(inference_session):
-        video_res_masks = processor.post_process_masks(
-            [sam2_video_output.pred_masks], original_sizes=[[inference_session.video_height, inference_session.video_width]], binarize=False
-        )[0]
-        video_segments[sam2_video_output.frame_idx] = video_res_masks
+#     video_segments = {}
+#     for sam2_video_output in model.propagate_in_video_iterator(inference_session):
+#         video_res_masks = processor.post_process_masks(
+#             [sam2_video_output.pred_masks], original_sizes=[[inference_session.video_height, inference_session.video_width]], binarize=False
+#         )[0]
+#         video_segments[sam2_video_output.frame_idx] = video_res_masks
+
+
+
+
+# import os
+# import numpy as np
+# import torch
+# import matplotlib.pyplot as plt
+# from PIL import Image
+
+# # select the device for computation
+# if torch.cuda.is_available():
+#     device = torch.device("cuda")
+# elif torch.backends.mps.is_available():
+#     device = torch.device("mps")
+# else:
+#     device = torch.device("cpu")
+# print(f"using device: {device}")
+
+# if device.type == "cuda":
+#     # use bfloat16 for the entire notebook
+#     torch.autocast("cuda", dtype=torch.bfloat16).__enter__()
+#     # turn on tfloat32 for Ampere GPUs (https://pytorch.org/docs/stable/notes/cuda.html#tensorfloat-32-tf32-on-ampere-devices)
+#     if torch.cuda.get_device_properties(0).major >= 8:
+#         torch.backends.cuda.matmul.allow_tf32 = True
+#         torch.backends.cudnn.allow_tf32 = True
+# elif device.type == "mps":
+#     print(
+#         "\nSupport for MPS devices is preliminary. SAM 2 is trained with CUDA and might "
+#         "give numerically different outputs and sometimes degraded performance on MPS. "
+#         "See e.g. https://github.com/pytorch/pytorch/issues/84936 for a discussion."
+#     )
+
+
+# np.random.seed(3)
+
+# def show_anns(anns, borders=True):
+#     if len(anns) == 0:
+#         return
+#     sorted_anns = sorted(anns, key=(lambda x: x['area']), reverse=True)
+#     ax = plt.gca()
+#     ax.set_autoscale_on(False)
+
+#     img = np.ones((sorted_anns[0]['segmentation'].shape[0], sorted_anns[0]['segmentation'].shape[1], 4))
+#     img[:, :, 3] = 0
+#     for ann in sorted_anns:
+#         m = ann['segmentation']
+#         color_mask = np.concatenate([np.random.random(3), [0.5]])
+#         img[m] = color_mask 
+#         if borders:
+#             import cv2
+#             contours, _ = cv2.findContours(m.astype(np.uint8), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE) 
+#             # Try to smooth contours
+#             contours = [cv2.approxPolyDP(contour, epsilon=0.01, closed=True) for contour in contours]
+#             cv2.drawContours(img, contours, -1, (0, 0, 1, 0.4), thickness=1) 
+
+#     ax.imshow(img)
+
+# image = Image.open(r'C:\Users\lahir\Downloads\cars.png')
+# image = np.array(image.convert("RGB"))
+
+
+
+# plt.figure(figsize=(20, 20))
+# plt.imshow(image)
+# plt.axis('off')
+# plt.show()
+
+
+# import torch
+# from sam2.build_sam import build_sam2
+# from sam2.sam2_image_predictor import SAM2ImagePredictor
+
+# checkpoint = r"C:\Users\lahir\code\segment-anything-2\checkpointssam2.1_hiera_large.pt"
+# model_cfg = r"C:\Users\lahir\code\segment-anything-2\sam2\configs\sam2.1\sam2.1_hiera_l.yaml"
+# predictor = SAM2ImagePredictor(build_sam2(model_cfg, checkpoint))
+
+
+
+
+
 
 
 
