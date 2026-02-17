485749df20ad35a5538b291edde1edc48d72ecc2
sleekEagle <lahirunuwanisme@gmail.com>
2026-02-17 12:35:33 +0530

    warpoed image sucessfully

--- motion_saliency.py
+++ motion_saliency.py
@@ -7,6 +7,7 @@ import numpy as np
 import cv2
 import os
 import sam_ui
+import random
 
 #create model and data loader
 ucf101dm = func.UCF101_data_model()
@@ -54,183 +55,226 @@ def show_images_seq(img1, img2):
     plt.ioff()
     plt.show()
 
-def spacial_analysis(video, frame_pairs):
-    gmodel.zero_grad()
-    input = video.permute(1,0,2,3)[None,:]
-    input.requires_grad = True
-    input = input.to('cuda')
-    input.retain_grad()  
-    pred = gmodel(input)
-    pred_idx = torch.argmax(pred,dim=1)
-    pred[0,pred_idx].backward()
-    ret = gmodel.calc_flow_saliency(input, frame_pairs, grad_method='gradcam')
-    return ret
+# def spacial_analysis(video, frame_pairs):
+#     gmodel.zero_grad()
+#     input = video.permute(1,0,2,3)[None,:]
+#     input.requires_grad = True
+#     input = input.to('cuda')
+#     input.retain_grad()  
+#     pred = gmodel(input)
+#     pred_idx = torch.argmax(pred,dim=1)
+#     pred[0,pred_idx].backward()
+#     ret = gmodel.calc_flow_saliency(input, frame_pairs, grad_method='gradcam')
+#     return ret
 
-def spacial_analysis_perturb(video, gt_class_idx, pred_logit, ordered_keys, clustered_ids):
+# def spacial_analysis_perturb(video, gt_class_idx, pred_logit, ordered_keys, clustered_ids):
 
-    #get gradcam mask for the input video
-    GCAM_THR = 0.2
-    gcam = gmodel.calc_gradcam(video[None,:].to('cuda'))
-    gcam_mask = (gcam > GCAM_THR).int()
-    # func.show_gray_image(gcam_mask[0,4,:].detach().cpu().numpy())
-    pairs = func.get_motion_pairs(clustered_ids)
+#     #get gradcam mask for the input video
+#     GCAM_THR = 0.2
+#     gcam = gmodel.calc_gradcam(video[None,:].to('cuda'))
+#     gcam_mask = (gcam > GCAM_THR).int()
+#     # func.show_gray_image(gcam_mask[0,4,:].detach().cpu().numpy())
+#     pairs = func.get_motion_pairs(clustered_ids)
 
-    heatmaps = []
-    for p in pairs:
-        out_dict = {}
-        p_ind = {}
-        ind = 0
-        for k in clustered_ids:
-            val = clustered_ids[k]
-            ind_ar = []
-            for i in range(len(val)):
-                ind_ar.append(ind)
-                ind+=1
-            p_ind[k] = ind_ar
+#     heatmaps = []
+#     for p in pairs:
+#         out_dict = {}
+#         p_ind = {}
+#         ind = 0
+#         for k in clustered_ids:
+#             val = clustered_ids[k]
+#             ind_ar = []
+#             for i in range(len(val)):
+#                 ind_ar.append(ind)
+#                 ind+=1
+#             p_ind[k] = ind_ar
 
-        p0 = p_ind[str(p[0])][0]
-        p1 = p_ind[str(p[1])][0]
+#         p0 = p_ind[str(p[0])][0]
+#         p1 = p_ind[str(p[1])][0]
 
-        img1 = video[:,p0,:][None,:]
-        img2 = video[:,p1,:][None,:]
-        flow = raftof.predict_flow_batch(img2, img1)
-        flow = raftof.resize_flow_interpolate(flow)
-        flow_mag = torch.sum(flow**2, dim=1)**0.5
-
-
-        out_dict['flow_mag'] = flow_mag[0,:].detach().cpu().numpy()
-        #check if this pair has sufficient motion
-
-        # v = torch.cat((img1,img2), dim=0)
-        # v = F.interpolate(v, size=(512,512), mode='bilinear', align_corners=False)
-
-        # func.play_tensor_video_opencv(v.permute(0,2,3,1),fps=1)
+#         img1 = video[:,p0,:][None,:]
+#         img2 = video[:,p1,:][None,:]
+#         flow = raftof.predict_flow_batch(img2, img1)
+#         flow = raftof.resize_flow_interpolate(flow)
+#         flow_mag = torch.sum(flow**2, dim=1)**0.5
 
 
-        # threshold = flow_mag.mean()
-        # flow_mask = (flow_mag > threshold).int()
-        # out_dict['flow_mask'] = flow_mask[0,:].detach().cpu().numpy()
-        # flow_mask = torch.ones_like(flow_mask)
-        # func.show_gray_image(mask[0,:].detach().cpu().numpy())
-        # func.show_gray_image(flow_mag[0,:].detach().cpu().numpy())
+#         out_dict['flow_mag'] = flow_mag[0,:].detach().cpu().numpy()
+#         #check if this pair has sufficient motion
 
-        # func.show_rgb_image(img1[0,:].permute(1,2,0).detach().cpu().numpy())
+#         # v = torch.cat((img1,img2), dim=0)
+#         # v = F.interpolate(v, size=(512,512), mode='bilinear', align_corners=False)
 
-        # mask = gcam_mask[0,clustered_ids[str(p[0])][0],:] * flow_mask
-        gc = gcam[0,clustered_ids[str(p[0])][0],:]
-        out_dict['gcam'] = gc.detach().cpu().numpy()
-        # out_dict['gcam_mask'] = gcam_mask[0,clustered_ids[str(p[0])][0],:].detach().cpu().numpy()
-        # out_dict['mask'] = mask[0,:].detach().cpu().numpy()
-        # mask_perc = torch.nonzero(mask).size(0)/(mask.size(1)*mask.size(2))
-        # assert mask_perc>=0.05, "Not enough pixels display significant motion"
-
-        mask = flow_mag[0,:]*gc
-        v = mask>mask.mean()
-        mask[v] = 1
-        mask[~v] = 0
-        out_dict['mask'] = mask.detach().cpu().numpy()
-
-        # func.show_gray_image(mask.detach().cpu().numpy())
-
-        # func.show_gray_image(gcam_mask[0,clustered_ids[str(p[0])][0],:].detach().cpu().numpy())
-        # func.show_rgb_image(img1[0,:].permute(1,2,0).detach().cpu().numpy())
-        # v = torch.cat((img1,img2), dim=0)
-        # func.play_tensor_video_opencv(v,fps=1)
-
-        ret = modify_flow(img1, img2, flow, mask)
-        warped = ret['warped']
-        mask = ret['mask']
-        window = ret['window']
-
-        videos = torch.empty(0).to(warped.device)
-        for i in range(warped.size(0)):
-            v = func.replace_frame(video, ordered_keys, clustered_ids, p[1], warped[i,:])
-            videos = torch.cat((videos, v[None,:].to(warped.device)), dim=0)
-        with torch.no_grad():
-            pred = model(videos)
-            pred = F.softmax(pred, dim=1)
-            logits = pred[:,gt_class_idx]
-        imp = pred_logit - logits
-        imp = (imp - imp.min())/(imp.max()-imp.min()+1e-5)
-        imp = imp[None,None,:].repeat(window,window,1)
-
-        hm = torch.zeros(1, 1, img2.size(2), img2.size(3))
-        hm_w = get_windows(hm, window, window)[0,0,:]
-        hm_w[...,mask.to(hm_w.device)] = imp.to(hm_w.device)
-
-        heatmap = hm_w[0,0,:]
-        heatmap = F.interpolate(heatmap[None,None,:], size=(112,112), mode='bilinear', align_corners=False)
-        heatmap = heatmap**1
-        heatmap = (heatmap - heatmap.min())/(heatmap.max()-heatmap.min()+1e-5)
-        heatmap = -1*heatmap+1
-        transformed = np.uint8(heatmap[0,0,:].numpy()*255)
-        h_col = cv2.applyColorMap(transformed, cv2.COLORMAP_JET)
-
-        img = np.uint8(((img2 - img2.min())/(img2.max() - img2.min()+1e-5)*255)[0,:].permute(1,2,0))
-
-        final_img = cv2.addWeighted(img, 0.6, h_col, 0.4, 0)
-        final_img = cv2.cvtColor(final_img, cv2.COLOR_RGB2BGR)
-        out_dict['heatmap'] = final_img
-        heatmaps.append(out_dict)
-        # cv2.imwrite('output_opencv.jpg', final_img)
-        # func.show_rgb_image(final_img)
-    return heatmaps
+#         # func.play_tensor_video_opencv(v.permute(0,2,3,1),fps=1)
 
 
-def get_windows(t, window_w, stride):
-    B,C,H,W = t.size()
-    windows = F.unfold(t, kernel_size=(window_w, window_w), stride=stride)
-    windows = windows.view(B, C, window_w, window_w, int(H/window_w), int(H/window_w))
-    return windows
+#         # threshold = flow_mag.mean()
+#         # flow_mask = (flow_mag > threshold).int()
+#         # out_dict['flow_mask'] = flow_mask[0,:].detach().cpu().numpy()
+#         # flow_mask = torch.ones_like(flow_mask)
+#         # func.show_gray_image(mask[0,:].detach().cpu().numpy())
+#         # func.show_gray_image(flow_mag[0,:].detach().cpu().numpy())
+
+#         # func.show_rgb_image(img1[0,:].permute(1,2,0).detach().cpu().numpy())
+
+#         # mask = gcam_mask[0,clustered_ids[str(p[0])][0],:] * flow_mask
+#         gc = gcam[0,clustered_ids[str(p[0])][0],:]
+#         out_dict['gcam'] = gc.detach().cpu().numpy()
+#         # out_dict['gcam_mask'] = gcam_mask[0,clustered_ids[str(p[0])][0],:].detach().cpu().numpy()
+#         # out_dict['mask'] = mask[0,:].detach().cpu().numpy()
+#         # mask_perc = torch.nonzero(mask).size(0)/(mask.size(1)*mask.size(2))
+#         # assert mask_perc>=0.05, "Not enough pixels display significant motion"
+
+#         mask = flow_mag[0,:]*gc
+#         v = mask>mask.mean()
+#         mask[v] = 1
+#         mask[~v] = 0
+#         out_dict['mask'] = mask.detach().cpu().numpy()
+
+#         # func.show_gray_image(mask.detach().cpu().numpy())
+
+#         # func.show_gray_image(gcam_mask[0,clustered_ids[str(p[0])][0],:].detach().cpu().numpy())
+#         # func.show_rgb_image(img1[0,:].permute(1,2,0).detach().cpu().numpy())
+#         # v = torch.cat((img1,img2), dim=0)
+#         # func.play_tensor_video_opencv(v,fps=1)
+
+#         ret = modify_flow(img1, img2, flow, mask)
+#         warped = ret['warped']
+#         mask = ret['mask']
+#         window = ret['window']
+
+#         videos = torch.empty(0).to(warped.device)
+#         for i in range(warped.size(0)):
+#             v = func.replace_frame(video, ordered_keys, clustered_ids, p[1], warped[i,:])
+#             videos = torch.cat((videos, v[None,:].to(warped.device)), dim=0)
+#         with torch.no_grad():
+#             pred = model(videos)
+#             pred = F.softmax(pred, dim=1)
+#             logits = pred[:,gt_class_idx]
+#         imp = pred_logit - logits
+#         imp = (imp - imp.min())/(imp.max()-imp.min()+1e-5)
+#         imp = imp[None,None,:].repeat(window,window,1)
+
+#         hm = torch.zeros(1, 1, img2.size(2), img2.size(3))
+#         hm_w = get_windows(hm, window, window)[0,0,:]
+#         hm_w[...,mask.to(hm_w.device)] = imp.to(hm_w.device)
+
+#         heatmap = hm_w[0,0,:]
+#         heatmap = F.interpolate(heatmap[None,None,:], size=(112,112), mode='bilinear', align_corners=False)
+#         heatmap = heatmap**1
+#         heatmap = (heatmap - heatmap.min())/(heatmap.max()-heatmap.min()+1e-5)
+#         heatmap = -1*heatmap+1
+#         transformed = np.uint8(heatmap[0,0,:].numpy()*255)
+#         h_col = cv2.applyColorMap(transformed, cv2.COLORMAP_JET)
+
+#         img = np.uint8(((img2 - img2.min())/(img2.max() - img2.min()+1e-5)*255)[0,:].permute(1,2,0))
+
+#         final_img = cv2.addWeighted(img, 0.6, h_col, 0.4, 0)
+#         final_img = cv2.cvtColor(final_img, cv2.COLOR_RGB2BGR)
+#         out_dict['heatmap'] = final_img
+#         heatmaps.append(out_dict)
+#         # cv2.imwrite('output_opencv.jpg', final_img)
+#         # func.show_rgb_image(final_img)
+#     return heatmaps
 
 
-'''
-img1: 1,3,H,W
-img2: 1,3,H,W
-flow: 1,2,H,W
-mask: H,W
-'''
-def modify_flow(img1, img2, flow, mask):
-    FLOW_RATIO = 0.8
-    window_w = 8
-    MASK_THR = 0.8
+# def get_windows(t, window_w, stride):
+#     B,C,H,W = t.size()
+#     windows = F.unfold(t, kernel_size=(window_w, window_w), stride=stride)
+#     windows = windows.view(B, C, window_w, window_w, int(H/window_w), int(H/window_w))
+#     return windows
 
-    if img1.size(2)!=flow.size(2):
-        img1 = F.interpolate(img1, size=(flow.size(2), flow.size(2)), mode='bilinear', align_corners=False)[0,:]
-        img2 = F.interpolate(img2, size=(flow.size(2), flow.size(2)), mode='bilinear', align_corners=False)[0,:]
-    if mask.size(0)!=flow.size(2):
-        mask = F.interpolate(mask[None,:].float(), size=(flow.size(2), flow.size(2)), mode='bilinear', align_corners=False)[0,0,:].int()
+
+# '''
+# img1: 1,3,H,W
+# img2: 1,3,H,W
+# flow: 1,2,H,W
+# mask: H,W
+# '''
+# def modify_flow(img1, img2, flow, mask):
+#     FLOW_RATIO = 0.8
+#     window_w = 8
+#     MASK_THR = 0.8
+
+#     if img1.size(2)!=flow.size(2):
+#         img1 = F.interpolate(img1, size=(flow.size(2), flow.size(2)), mode='bilinear', align_corners=False)[0,:]
+#         img2 = F.interpolate(img2, size=(flow.size(2), flow.size(2)), mode='bilinear', align_corners=False)[0,:]
+#     if mask.size(0)!=flow.size(2):
+#         mask = F.interpolate(mask[None,:].float(), size=(flow.size(2), flow.size(2)), mode='bilinear', align_corners=False)[0,0,:].int()
     
-    mask_w = get_windows(mask[None,None,:].float(), window_w, window_w).long()[0,0,:]
-    mask_mean = (mask_w.sum(dim=(0,1))/(mask_w.size(0)**2))
-    mask_mask = mask_mean > MASK_THR
+#     mask_w = get_windows(mask[None,None,:].float(), window_w, window_w).long()[0,0,:]
+#     mask_mean = (mask_w.sum(dim=(0,1))/(mask_w.size(0)**2))
+#     mask_mask = mask_mean > MASK_THR
 
-    #create modified flow
-    flow_w = get_windows(flow, window_w, window_w)[0,:]
-    flow_w_mod = flow_w[...,mask_mask]*FLOW_RATIO
-    valid_mask_idx = torch.nonzero(mask_mask)
-    flow_batch = flow.repeat(valid_mask_idx.size(0),1,1,1)
-    for i in range(valid_mask_idx.size(0)):
-        idx = valid_mask_idx[i]
-        f_mod_ = flow_w_mod[:,:,:,i]
-        h_, w_ = idx[0]*window_w, idx[1]*window_w
-        flow_batch[i,:,h_:h_+window_w,w_:w_+window_w] = f_mod_
+#     #create modified flow
+#     flow_w = get_windows(flow, window_w, window_w)[0,:]
+#     flow_w_mod = flow_w[...,mask_mask]*FLOW_RATIO
+#     valid_mask_idx = torch.nonzero(mask_mask)
+#     flow_batch = flow.repeat(valid_mask_idx.size(0),1,1,1)
+#     for i in range(valid_mask_idx.size(0)):
+#         idx = valid_mask_idx[i]
+#         f_mod_ = flow_w_mod[:,:,:,i]
+#         h_, w_ = idx[0]*window_w, idx[1]*window_w
+#         flow_batch[i,:,h_:h_+window_w,w_:w_+window_w] = f_mod_
 
-    #warp the image with the modified flow
-    warp = func.warp_batch(img1.repeat(valid_mask_idx.size(0),1,1,1).to('cuda'),flow_batch)
+#     #warp the image with the modified flow
+#     warp = func.warp_batch(img1.repeat(valid_mask_idx.size(0),1,1,1).to('cuda'),flow_batch)
 
-    ret = {
-        'warped': warp,
-        'window': window_w,
-        'mask': mask_mask,
-    }
+#     ret = {
+#         'warped': warp,
+#         'window': window_w,
+#         'mask': mask_mask,
+#     }
 
-    return ret
+#     return ret
 
 
 # func.show_gray_image(mask_mask.float().cpu().numpy())
 
-import random
+from skimage.transform import resize
+
+def spacial_analysis(video, pairs, masks, clustered_ids):
+    FLOW_RATIO = 0.9
+    ordered_keys = list(dict(sorted(clustered_ids.items(), key=lambda x: x[1][0])).keys())
+    video = func.create_new_video(video.permute(0,2,3,1), clustered_ids, ordered_keys)
+
+    #predict flow batch
+
+    img1 = torch.empty(0)
+    img2 = torch.empty(0)
+    for pair in pairs:
+        i1_ = video[pair[0],:][None,:].permute(0,3,1,2)
+        i2_ = video[pair[1],:][None,:].permute(0,3,1,2)
+        img1 = torch.concatenate([img1,i1_],dim=0)
+        img2 = torch.concatenate([img2,i2_],dim=0)
+    flow = raftof.predict_flow_batch(img2, img1)
+    flow = raftof.resize_flow_interpolate(flow)
+
+    for i, pair in enumerate(pairs):
+        m = masks[pair[0]]
+        f = flow[i,:]
+        for oi in m.keys():
+            obj_mask = m[oi][0,:]
+            resized = resize(obj_mask.astype(float), (img1.size(2),img1.size(3)), preserve_range=True)
+            obj_mask = resized > 0.5
+
+            #modify flow
+            fcopy = f.clone()
+            f_ = fcopy*FLOW_RATIO
+            fcopy[:,obj_mask] = f_[:,obj_mask]
+
+            #warp image to generate modified image
+            warp = func.warp_batch(img1.to('cuda'),fcopy)
+
+
+            pass
+
+
+
+
+    pass
+
+
 def go_through_samples():
     mask_out_path = r'C:\Users\lahir\Downloads\UCF101\analysis\masks'
     path = r'C:\Users\lahir\Downloads\UCF101\analysis\motion_importance.json'
@@ -250,6 +294,7 @@ def go_through_samples():
         #     continue
         #consider only samples that are correcly predicted
         gt_class = d['motion_importance']['gt_class']
+        print(f'Class: {gt_class}')
         gt_class_idx = class_labels[gt_class.lower()]
         pred_logit = d['motion_importance']['pred_original_logit']
         pred_class = d['motion_importance']['pred_original_class']
@@ -291,7 +336,7 @@ def go_through_samples():
 
             #CHECK IF MASKS ALREADY GENERATED
             maks_exist = False
-            mask_path = os.path.join(mask_out_path, k)
+            mask_path = os.path.join(mask_out_path, gt_class, k)
             if os.path.exists(mask_path):
                 if len(os.listdir(mask_path)) == 16:
                     maks_exist = True
@@ -308,7 +353,7 @@ def go_through_samples():
                         o_path = os.path.join(f_path, f'{oi}.jpg')
                         cv2.imwrite(o_path, om[0,:]*255)
             
-            pass
+            spacial_analysis(video, pairs, masks, clustered_ids)
 
             # video_ = func.create_new_video(video.permute(1,0,2,3), clustered_ids, ordered_keys)
             # heatmaps = spacial_analysis_perturb(video_, gt_class_idx, d['pair_analysis']['all_imp_pairs_logit'], ordered_keys, clustered_ids)
